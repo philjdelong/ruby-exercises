0d07e01ad799d4495280e0a934a2f8380668c000
Phil DeLong <philjdelong@gmail.com>
2019-10-12 10:13:55 -0600

    Add require adult.rb

--- command-query/exercises/adult_test.rb
+++ command-query/exercises/adult_test.rb
@@ -1,7 +1,7 @@
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative 'adult'
+require './lib/adult'
 
 class AdultTest < Minitest::Test
   def test_adult_is_sober
