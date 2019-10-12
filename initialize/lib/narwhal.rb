63afb84fed45915520723636f46d8eb2001591aa
Phil DeLong <philjdelong@gmail.com>
2019-10-12 10:06:59 -0600

    Initial commit

---
+++ initialize/lib/narwhal.rb
@@ -0,0 +1,16 @@
+require'pry'
+
+class Narwhal
+  attr_reader :cute, :weight, :name
+
+  def initialize(name)
+    @cute = cute
+    @weight = weight
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+end
