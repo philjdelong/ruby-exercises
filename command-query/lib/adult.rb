505030571ba4fe551196940e8ed8a2f86def1d86
Phil DeLong <philjdelong@gmail.com>
2019-10-12 10:34:42 -0600

    Add consume alcoholic beverage method

--- command-query/lib/adult.rb
+++ command-query/lib/adult.rb
@@ -1,7 +1,29 @@
 class Adult
 
-  def sober?
-    p "Adult should really be sober right now."
+  def initialize(drinks)
+    @drinks = 0
   end
-  
+
+  def consume_an_alcoholic_beverage
+     @drinks += 1
+  end
+
+  def sober?
+    if
+      @drinks = 0
+      p "Adult should really be sober right now."
+    else if
+      @drinks = 1
+      p "Still sober."
+    else if
+      @drinks = 2
+      p "Not drunk yet."
+    else if
+      @drinks = 3
+      p "Yeah, OK. The adult is drunk."
+    else
+      p "The adult doesn't get more sober from drinking more."
+    end
+  end
+
 end
